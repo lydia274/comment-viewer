6cbb06776de9303e8501f8f249c3184000602af2
lydia a <lydie.antonova@gmail.com>
2023-07-30 13:56:31 +0200

    connected comments with some sort of styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,17 @@
 import React from "react"
+import Body from "./Body"
+import Header from "./Header"
 
-function Card() {
-  return <div>Card</div>
+function Card(props) {
+  return (
+    <div>
+      <Header
+        profileImg={props.commentObject.profileImg}
+        username={props.commentObject.username}
+      />
+      <Body comment={props.commentObject.comment} />
+    </div>
+  )
 }
 
 export default Card
