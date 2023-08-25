b5c95b6ad9aa51d683c0cc951e471d6e260af6a8
lydia a <lydie.antonova@gmail.com>
2023-08-25 17:32:30 +0200

    modal done

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -24,10 +24,38 @@ const Modal = ({ user, onClose }) => {
         <span className="close" onClick={onClose}>
           &times;
         </span>
-        <h2>{user.last_name}</h2>
-        <p>{user.employment.title}</p>
-        <p>{user.employment.key_skill}</p>
-        {/* Render other user details as needed */}
+
+        <div className="portrait-container">
+          <div>
+            <img src={user.avatar} id="portrait-pic" />
+          </div>
+          <div className="portrait-title">
+            <h2>
+              {user.first_name} {user.last_name}
+            </h2>
+            <p>Born on {user.date_of_birth}</p>
+          </div>
+        </div>
+
+        <div id="profile-intro">
+          <p>
+            Meet {user.first_name} {user.last_name}, a dynamic extraterrestral{" "}
+            {user.gender} individual from {user.address.city}, state{" "}
+            {user.address.state}, {user.address.country}.
+          </p>
+          <p>
+            As a {user.employment.title}, {user.first_name} is known for their
+            tremendous {user.employment.key_skill} skills.
+          </p>
+          <p>
+            Enthusiastic explorer of {user.address.country}'s landscapes,{" "}
+            {user.first_name} finds peace in nature's embrace.
+          </p>
+          <p>
+            Reach out to {user.first_name} via email at {user.email} or on the
+            phone at {user.phone_number}.
+          </p>
+        </div>
       </div>
     </div>
   )
