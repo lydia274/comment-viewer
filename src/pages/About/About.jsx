7725f305a4781c96c3d65ad01684f299446d1475
lydia a <lydie.antonova@gmail.com>
2023-08-29 17:05:25 +0200

    updated about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -9,7 +9,10 @@ export default function About() {
           title="About the project"
           body="The project is built on React JS, using API to fetch data from https://random-data-api.com/"
         />
-        <Article body="Easter Egg! Hover the color band in the header to see  its background change" />
+        <Article
+          title="Easter Egg!"
+          body=" Hover the color band in the header to see  its background change"
+        />
       </div>
     </>
   )
