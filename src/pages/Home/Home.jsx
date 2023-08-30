9f350e8f0408796b53107b27821e0fbaf62f55b7
lydia a <lydie.antonova@gmail.com>
2023-08-30 15:44:54 +0200

    cleanup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,4 @@
 import React from "react"
-// import Header from "../../components/Header/Header"
-import { Link } from "react-router-dom"
 import User from "../../components/User/User"
 
 function Home() {
